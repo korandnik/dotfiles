4996d06444ae08b5bf0793243ade41c539f639e7
Andrei Korotchin <korandnik2006@gmail.com>
2026-04-18 06:11:05 +0300

    hypr: hyprpaper: formatting + removed splash

--- hypr/.config/hypr/bin/hyprpaper.sh
+++ hypr/.config/hypr/bin/hyprpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
 # Get the name of the focused monitor with hyprctl
 FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
 # Get a random wallpaper that is not the current one
